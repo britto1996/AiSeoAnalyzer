4a205db765db29bb8f92d246dd85016976ae73ff
britto1996 <btm15037@gmail.com>
2026-05-30 19:08:59 +0400

    Feat: Hero ui updated.

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -2,54 +2,60 @@
 
 import { paths } from "@/app/constants/paths";
 import { useApp } from "@/app/context/AppContext";
+import { X } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const {user, loading} = useApp();
+  const { user } = useApp();
+  const router = useRouter();
 
-  const closeNavbar = () => {
-    setIsMenuOpen(false);
+  const navigateToLogin = () => {
+    router.push(paths.login);
   };
 
-  console.log(user, loading);
-
   return (
-    <nav className="flex items-center justify-between p-4 md:px-16 lg:px-24 xl:px-32 border-b border-white/25 w-full">
-      <a href="#">
-        <Image
-          unoptimized
-          src="/logo/seo_rank_gif.gif"
-          alt="Site Name Logo"
-          width={157}
-          height={157}
-          loading="eager"
-        />
-      </a>
-      <ul
-        id="menu"
-        className={`max-md:absolute max-md:h-full max-md:z-50 max-md:w-full max-md:top-0 transition-all duration-300 max-md:backdrop-blur max-md:bg-white/70 max-md:text-base flex flex-col md:flex-row items-center justify-center gap-8 font-medium ${isMenuOpen ? "max-md:left-0" : "max-md:-left-full"}`}
-      >
-        <li onClick={closeNavbar} className="hover:text-slate-500">
-          <a href={user !== null ? paths.dashboard : paths.login}>Dashboard</a>
-        </li>
-        <li onClick={closeNavbar} className="hover:text-slate-500">
-          <a href="#">Analyze</a>
-        </li>
-        <li onClick={closeNavbar} className="hover:text-slate-500">
-          <a href="#">Rank Tracker</a>
-        </li>
-        <li onClick={closeNavbar} className="hover:text-slate-500">
-          <a href="#">History</a>
-        </li>
-
-        <button
-          id="close-menu"
-          onClick={closeNavbar}
-          className="md:hidden bg-gray-800 hover:bg-black text-white p-2 rounded-md aspect-square font-medium transition"
-        >
+    <nav className="h-20">
+      <div className="fixed left-0 top-0 right-0 z-100 flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-2 border-b border-gray-300 bg-white transition-all">
+        <a href="#">
+          <Image
+            unoptimized
+            src="/logo/seo_rank_gif.gif"
+            alt="Site Name Logo"
+            width={64}
+            height={64}
+            loading="eager"
+            className="object-contain"
+          />
+        </a>
+        <div className="hidden sm:flex items-center gap-5 md:gap-7 text-xs text-gray-500 font-medium tracking-wide">
+          <a
+            onClick={() => scrollTo(0, 0)}
+            href={user !== null ? paths.dashboard : paths.login}
+          >
+            Dashboard
+          </a>
+          <a onClick={() => scrollTo(0, 0)} href="#">
+            Analyze
+          </a>
+          <a onClick={() => scrollTo(0, 0)} href="#">
+            Rank Tracker
+          </a>
+          <a onClick={() => scrollTo(0, 0)} href="#">
+            History
+          </a>
+        </div>
+        <div>
+          <button
+            onClick={() => navigateToLogin()}
+            className="max-sm:hidden cursor-pointer px-5 py-1.5 text-sm bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full"
+          >
+            Login
+          </button>
           <svg
+            onClick={() => setIsMenuOpen(true)}
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
@@ -59,34 +65,39 @@ const Header = () => {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
+            className="sm:hidden"
           >
-            <path d="M18 6 6 18" />
-            <path d="m6 6 12 12" />
+            <path d="M4 5h16" />
+            <path d="M4 12h16" />
+            <path d="M4 19h16" />
           </svg>
-        </button>
-      </ul>
-
-      <button
-        id="open-menu"
-        onClick={() => setIsMenuOpen(true)}
-        className="md:hidden"
+        </div>
+      </div>
+      <div
+        className={`sm:hidden fixed inset-0 ${isMenuOpen ? "w-full" : "w-0"} overflow-hidden bg-white backdrop-blur shadow-xl rounded-lg z-20 text-sm transition-all`}
       >
-        <svg
-          className="size-7"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </button>
-
-      <button className="max-md:hidden px-6 py-3 text-white bg-indigo-600 hover:bg-indigo-700 transition rounded-full">
-        Contact us
-      </button>
+        <div className="flex flex-col items-center justify-center h-full text-xl font-semibold gap-6 p-4">
+          <a
+            onClick={() => scrollTo(0, 0)}
+            href={user !== null ? paths.dashboard : paths.login}
+          >
+            Dashboard
+          </a>
+          <a onClick={() => scrollTo(0, 0)} href="#">
+            Analyze
+          </a>
+          <a onClick={() => scrollTo(0, 0)} href="#">
+            Rank Tracker
+          </a>
+          <a onClick={() => scrollTo(0, 0)} href="#">
+            History
+          </a>
+          <button className=" cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
+            Login
+          </button>
+          <X className="cursor-pointer text-indigo-500 hover:text-indigo-600 transition rounded-full font-bold" onClick={() => setIsMenuOpen(false)} />
+        </div>
+      </div>
     </nav>
   );
 };
--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -1,121 +1,90 @@
+"use client";
+
 import { heroImages } from "@/app/constants/heroImages";
-import { paths } from "@/app/constants/paths";
 import Image from "next/image";
-import Link from "next/link";
+import { useState } from "react";
+import SearchBox from "../SearchBox";
 
 const Hero = () => {
+  const [input, setInput] = useState("");
   return (
-    <div className="flex-1 flex flex-col-reverse gap-10 md:flex-row md:items-center max-md:justify-center px-4 lg:ml-10 md:px-16 lg:px-24 xl:px-32 py-12 md:py-0">
-      <div className="max-md:text-center md:flex-1">
-        <h5 className="text-3xl md:text-6xl/[76px] font-semibold max-w-xl bg-linear-to-r from-slate-900 to-[#6D8FE4] text-transparent bg-clip-text">
-          Analyze & Boost Your SEO Rankings
-        </h5>
-        <p className="text-sm md:text-base max-w-lg mt-6 max-md:px-2 text-slate-600">
-          Get instant AI-powered SEO audits for any website. Uncover hidden issues, optimize performance, and outrank your competition.
-        </p>
-        <div className="flex items-center gap-4 mt-6 max-md:justify-center">
-          <Link href={paths.login}>
-          <button
-            className="px-8 cursor-pointer py-3 rounded-md bg-indigo-600 hover:bg-indigo-700 text-white active:scale-95 transition-all"
-            type="button"
-          >
-            Get Started
-          </button>
-          </Link>
+    <div className="rethink relative flex flex-col items-center justify-center text-sm px-4 md:px-16 lg:px-24 xl:px-40 text-gray-800">
+      <div className="flex items-center mt-24 md:mt-36">
+        <div className="flex -space-x-3 pr-3">
+          {heroImages?.map((image, index) => {
+            return (
+              <Image
+                key={index}
+                src={image}
+                alt={`Hero Image ${index + 1}`}
+                width={40}
+                height={40}
+                loading="eager"
+                className="size-10 border-2 border-white rounded-full hover:-translate-y-px transition z-1"
+              />
+            );
+          })}
         </div>
-        <div className="flex items-center mt-9 max-md:justify-center">
-          <div className="flex -space-x-3.5 pr-3">
-            {heroImages?.map((image, index) => {
-              return (
-                <Image
-                  key={index}
-                  src={image}
-                  alt={`Hero Image ${index + 1}`}
-                  width={40}
-                  height={40}
-                  loading="eager"
-                  className="size-10 border-2 border-white rounded-full hover:-translate-y-px transition z-1"
-                />
-              );
-            })}
+        <div>
+          <div className="flex">
+            {Array(5)
+              .fill(0)
+              .map((_, i) => (
+                <svg
+                  key={i}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-star text-transparent fill-indigo-600"
+                  aria-hidden="true"
+                >
+                  <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+                </svg>
+              ))}
           </div>
-          <div>
-            <div className="flex items-center gap-px">
-              <svg
-                width="13"
-                height="12"
-                viewBox="0 0 13 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.85536 0.463527C6.00504 0.00287118 6.65674 0.00287028 6.80642 0.463526L7.82681 3.60397C7.89375 3.80998 8.08572 3.94946 8.30234 3.94946H11.6044C12.0888 3.94946 12.2901 4.56926 11.8983 4.85397L9.22687 6.79486C9.05162 6.92219 8.97829 7.14787 9.04523 7.35388L10.0656 10.4943C10.2153 10.955 9.68806 11.338 9.2962 11.0533L6.62478 9.11244C6.44954 8.98512 6.21224 8.98512 6.037 9.11244L3.36558 11.0533C2.97372 11.338 2.44648 10.955 2.59616 10.4943L3.61655 7.35388C3.68349 7.14787 3.61016 6.92219 3.43491 6.79486L0.763497 4.85397C0.37164 4.56927 0.573027 3.94946 1.05739 3.94946H4.35944C4.57606 3.94946 4.76803 3.80998 4.83497 3.60397L5.85536 0.463527Z"
-                  fill="#FF8F20"
-                />
-              </svg>
-              <svg
-                width="13"
-                height="12"
-                viewBox="0 0 13 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.85536 0.463527C6.00504 0.00287118 6.65674 0.00287028 6.80642 0.463526L7.82681 3.60397C7.89375 3.80998 8.08572 3.94946 8.30234 3.94946H11.6044C12.0888 3.94946 12.2901 4.56926 11.8983 4.85397L9.22687 6.79486C9.05162 6.92219 8.97829 7.14787 9.04523 7.35388L10.0656 10.4943C10.2153 10.955 9.68806 11.338 9.2962 11.0533L6.62478 9.11244C6.44954 8.98512 6.21224 8.98512 6.037 9.11244L3.36558 11.0533C2.97372 11.338 2.44648 10.955 2.59616 10.4943L3.61655 7.35388C3.68349 7.14787 3.61016 6.92219 3.43491 6.79486L0.763497 4.85397C0.37164 4.56927 0.573027 3.94946 1.05739 3.94946H4.35944C4.57606 3.94946 4.76803 3.80998 4.83497 3.60397L5.85536 0.463527Z"
-                  fill="#FF8F20"
-                />
-              </svg>
-              <svg
-                width="13"
-                height="12"
-                viewBox="0 0 13 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.85536 0.463527C6.00504 0.00287118 6.65674 0.00287028 6.80642 0.463526L7.82681 3.60397C7.89375 3.80998 8.08572 3.94946 8.30234 3.94946H11.6044C12.0888 3.94946 12.2901 4.56926 11.8983 4.85397L9.22687 6.79486C9.05162 6.92219 8.97829 7.14787 9.04523 7.35388L10.0656 10.4943C10.2153 10.955 9.68806 11.338 9.2962 11.0533L6.62478 9.11244C6.44954 8.98512 6.21224 8.98512 6.037 9.11244L3.36558 11.0533C2.97372 11.338 2.44648 10.955 2.59616 10.4943L3.61655 7.35388C3.68349 7.14787 3.61016 6.92219 3.43491 6.79486L0.763497 4.85397C0.37164 4.56927 0.573027 3.94946 1.05739 3.94946H4.35944C4.57606 3.94946 4.76803 3.80998 4.83497 3.60397L5.85536 0.463527Z"
-                  fill="#FF8F20"
-                />
-              </svg>
-              <svg
-                width="13"
-                height="12"
-                viewBox="0 0 13 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.85536 0.463527C6.00504 0.00287118 6.65674 0.00287028 6.80642 0.463526L7.82681 3.60397C7.89375 3.80998 8.08572 3.94946 8.30234 3.94946H11.6044C12.0888 3.94946 12.2901 4.56926 11.8983 4.85397L9.22687 6.79486C9.05162 6.92219 8.97829 7.14787 9.04523 7.35388L10.0656 10.4943C10.2153 10.955 9.68806 11.338 9.2962 11.0533L6.62478 9.11244C6.44954 8.98512 6.21224 8.98512 6.037 9.11244L3.36558 11.0533C2.97372 11.338 2.44648 10.955 2.59616 10.4943L3.61655 7.35388C3.68349 7.14787 3.61016 6.92219 3.43491 6.79486L0.763497 4.85397C0.37164 4.56927 0.573027 3.94946 1.05739 3.94946H4.35944C4.57606 3.94946 4.76803 3.80998 4.83497 3.60397L5.85536 0.463527Z"
-                  fill="#FF8F20"
-                />
-              </svg>
-              <svg
-                width="13"
-                height="12"
-                viewBox="0 0 13 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.85536 0.463527C6.00504 0.00287118 6.65674 0.00287028 6.80642 0.463526L7.82681 3.60397C7.89375 3.80998 8.08572 3.94946 8.30234 3.94946H11.6044C12.0888 3.94946 12.2901 4.56926 11.8983 4.85397L9.22687 6.79486C9.05162 6.92219 8.97829 7.14787 9.04523 7.35388L10.0656 10.4943C10.2153 10.955 9.68806 11.338 9.2962 11.0533L6.62478 9.11244C6.44954 8.98512 6.21224 8.98512 6.037 9.11244L3.36558 11.0533C2.97372 11.338 2.44648 10.955 2.59616 10.4943L3.61655 7.35388C3.68349 7.14787 3.61016 6.92219 3.43491 6.79486L0.763497 4.85397C0.37164 4.56927 0.573027 3.94946 1.05739 3.94946H4.35944C4.57606 3.94946 4.76803 3.80998 4.83497 3.60397L5.85536 0.463527Z"
-                  fill="#FF8F20"
-                />
-              </svg>
-            </div>
-            <p className="text-sm text-slate-500">Used by 1,000+ people</p>
-          </div>
+          <p className="text-sm text-gray-700">Used by 10,000+ users.</p>
         </div>
       </div>
-      <div className="w-full md:max-w-xs lg:max-w-lg md:shrink-0">
-        <Image
-          className="w-full h-auto"
-          alt="Hero Image"
-          src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/hero/users-group.png"
-          width={500}
-          height={500}
-          loading="eager"
-        />
-      </div>
+      {/* Headline */}
+      <h1 className="text-4xl md:text-6xl font-semibold max-w-lg md:max-w-2xl text-center mt-4 leading-tight md:leading-tight">
+        Analyze & Boost Your{" "}
+        <span className="relative bg-linear-to-r from-purple-700 to-[#764de1] bg-clip-text text-transparent">
+          SEO Rankings
+          <div className="z-10 absolute bottom-0 left-0 w-full scale-120">
+            <Image
+              src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/hero/gradient_arc.svg"
+              alt="Gradient Arc"
+              width={400}
+              height={100}
+              loading="eager"
+            />
+          </div>
+        </span>
+        <span className="relative bg-linear-to-r from-[#764de1] to-indigo-600 bg-clip-text text-transparent">
+          {" " }with
+        </span>{" "}
+        AI-Powered Audits
+      </h1>
+      <p className="max-w-xl text-center text-base my-7">
+        Get instant AI-powered SEO audits for any website. Uncover hidden
+        issues, optimize performance, and outrank your competition.
+      </p>
+      <SearchBox
+        onSubmitHandler={(e) => {
+          e.preventDefault();
+          console.log(input);
+        }}
+        input={input}
+        setInput={setInput}
+        placeholder="Enter your website URL"
+        buttonTxt="Analyze"
+      />
     </div>
   );
 };
