1fa63f158492f1641d8aa46587a84d498e74e32c
britto1996 <btm15037@gmail.com>
2026-08-12 18:16:38 +0400

    ts issue resolved

--- app/components/Header/HeaderList/index.tsx
+++ app/components/Header/HeaderList/index.tsx
@@ -9,7 +9,7 @@ import {
 
 const HeaderList = [
   {
-    _id: 1,
+    _id: "1",
     title: "Home",
     icon: (
       <div className="bg-white hover:bg-linear-to-r hover:from-green-700 hover:to-blue-900 p-3 rounded-full hover:text-white text-indigo-500 cursor-pointer">
@@ -18,7 +18,7 @@ const HeaderList = [
     ),
   },
   {
-    _id: 2,
+    _id: "2",
     title: "Dashboard",
     icon: (
       <div className="bg-white p-3 rounded-full hover:bg-linear-to-r hover:from-green-700 hover:to-blue-900 hover:text-white text-indigo-500 cursor-pointer">
@@ -27,7 +27,7 @@ const HeaderList = [
     ),
   },
   {
-    _id: 3,
+    _id: "3",
     title: "Templates",
     icon: (
       <div className="bg-white hover:bg-linear-to-r hover:from-green-700 hover:to-blue-900 p-3 rounded-full hover:text-white text-indigo-500 cursor-pointer">
@@ -36,7 +36,7 @@ const HeaderList = [
     ),
   },
   {
-    _id: 4,
+    _id: "4",
     title: "Analyze",
     icon: (
       <div className="bg-white hover:bg-linear-to-r hover:from-green-700 hover:to-blue-900 p-3 rounded-full hover:text-white text-indigo-500 cursor-pointer">
@@ -45,7 +45,7 @@ const HeaderList = [
     ),
   },
   {
-    _id: 5,
+    _id: "5",
     title: "Rank Tracker",
     icon: (
       <div className="bg-white hover:bg-linear-to-r hover:from-green-700 hover:to-blue-900 p-3 rounded-full hover:text-white text-indigo-500 cursor-pointer">
@@ -54,7 +54,7 @@ const HeaderList = [
     ),
   },
   {
-    _id: 6,
+    _id: "6",
     title: "History",
     icon: (
       <div className="bg-white hover:bg-linear-to-r hover:from-green-700 hover:to-blue-900 p-3 rounded-full hover:text-white text-indigo-500 cursor-pointer">
